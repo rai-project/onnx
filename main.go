6d8ff3dadb313467549ef994f8767d8d28ed0847
Abdul Dakkak <abduld@wolfram.com>
2018-06-14 22:47:59 +0100

    update travis

--- main.go
+++ main.go
@@ -2,6 +2,9 @@
 
 package main
 
+// #cgo LDFLAGS: -lonnx -lonnx_proto -lprotobuf -lstdc++ -L${SRCDIR}/onnx/build -L/usr/local/lib
+// #cgo CXXFLAGS: -std=c++11 -I${SRCDIR}/onnx/build -I${SRCDIR}/onnx -O3 -Wall -g -DONNX_NAMESPACE=onnx
+// #cgo CXXFLAGS: -Wno-sign-compare -Wno-unused-function
 import "C"
 
 import (
