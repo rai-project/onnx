f13295bdd6fc0f06ed0d7faa400e78b60951ffaf
Cheng Li <cli99@illinois.edu>
2018-06-21 21:05:04 -0400

    work

---
+++ shape_infer_test.go
@@ -0,0 +1,42 @@
+package onnx
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/Unknwon/com"
+	"github.com/gogo/protobuf/proto"
+	"github.com/k0kubun/pp"
+
+	sourcepath "github.com/GeertJohan/go-sourcepath"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestModelShapeInfer ...
+func TestModelShapeInfer(t *testing.T) {
+
+	//onnxModelFile := filepath.Join(sourcepath.MustAbsoluteDir(), "_fixtures", "", "test.onnx")
+
+	onnxModelFile := filepath.Join(sourcepath.MustAbsoluteDir(), "_fixtures", "", "test.onnx")
+
+	model, err := ReadModelShapeInfer(onnxModelFile)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, model)
+
+	graph := model.GetGraph()
+
+	for _, val := range graph.GetValueInfo() {
+		pp.Println(val.GetType().GetValue())
+	}
+
+	buf, err := proto.Marshal(model)
+	com.WriteFile(filepath.Join(sourcepath.MustAbsoluteDir(), "_fixtures", "", "testa_inferred.onnx"), buf)
+
+	// assert.Equal(t, "Conv", nodes[0].GetOpType())
+	// assert.Equal(t, "Relu", nodes[1].GetOpType())
+
+	// assert.Equal(t, int64(3), model.GetIrVersion())
+	// assert.Equal(t, "CNTK", model.GetProducerName())
+	// assert.Equal(t, "Constant", nodes[0].GetOpType())
+	// assert.Equal(t, "Div", nodes[1].GetOpType())
+}
