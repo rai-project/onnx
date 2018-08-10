05b64e8b52129defb03db8005900424bb19bd61e
Abdul Dakkak <abduld@wolfram.com>
2018-08-10 15:08:20 -0400

    add optimize

--- cbits.hpp
+++ cbits.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 
 #ifdef __cplusplus
@@ -13,6 +14,25 @@ typedef struct {
 } go_string;
 
 go_string go_shape_inference(char *bytes, size_t len);
+
+go_string go_optimize(char *bytes, size_t len, char **optnames, int numopts);
+
+static char **makeCharArray(int size) { return (char **) calloc(sizeof(char *), size); }
+
+static void setArrayString(char **a, char *s, int n) { a[n] = s; }
+
+static void freeCharArray(char **a, int size) {
+  if (a == NULL) {
+    return;
+  }
+  for (int ii = 0; ii < size; ii++) {
+    if (a == NULL) {
+      continue;
+    }
+    free(a[ii]);
+  }
+  free(a);
+}
 
 #ifdef __cplusplus
 }
---
+++ optimize.go
@@ -0,0 +1,52 @@
+package onnx
+
+/*
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "cbits.hpp"
+*/
+import "C"
+
+import (
+	"io/ioutil"
+	"unsafe"
+
+	"github.com/Unknwon/com"
+	"github.com/gogo/protobuf/proto"
+	"github.com/pkg/errors"
+)
+
+func OptmizeModel(protoFileName string, optimizationNames []string) (*ModelProto, error) {
+
+	if !com.IsFile(protoFileName) {
+		return nil, errors.Errorf("%s is not a file", protoFileName)
+	}
+	buf, err := ioutil.ReadFile(protoFileName)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to open %s", protoFileName)
+	}
+
+	protoContent := C.CBytes(buf)
+	defer C.free(unsafe.Pointer(protoContent))
+
+	cargs := C.makeCharArray(C.int(len(optimizationNames)))
+	defer C.freeCharArray(cargs, C.int(len(optimizationNames)))
+	for i, s := range optimizationNames {
+		C.setArrayString(cargs, C.CString(s), C.int(i))
+	}
+
+	shapedProtoContentC := C.go_optimize((*C.char)(protoContent), C.size_t(len(buf)), cargs, C.int(len(optimizationNames)))
+	if shapedProtoContentC.buf == nil {
+		return nil, errors.Wrapf(err, "failed to shape infer %s", protoFileName)
+	}
+	length := C.int(shapedProtoContentC.length)
+	sharedProtoContent := C.GoBytes(unsafe.Pointer(shapedProtoContentC.buf), length)
+
+	defer C.free(unsafe.Pointer(shapedProtoContentC.buf))
+
+	model := new(ModelProto)
+	err = proto.Unmarshal(sharedProtoContent, model)
+
+	return model, err
+}
