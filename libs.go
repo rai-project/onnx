bd616f923a241e92e23638a57ce6a86533f753cb
Abdul Dakkak <abduld@wolfram.com>
2018-08-15 01:32:25 -0400

    update

--- libs.go
+++ libs.go
@@ -1,6 +1,7 @@
 package onnx
 
 // #cgo LDFLAGS: -lonnx -lonnx_proto -lprotobuf -lstdc++ -L${SRCDIR}/onnx/build -L/usr/local/lib
+// #cgo CFLAGS: -std=c++11 -I${SRCDIR}/onnx/build -I${SRCDIR}/onnx -O3 -Wall -g -DONNX_NAMESPACE=onnx
 // #cgo CXXFLAGS: -std=c++11 -I${SRCDIR}/onnx/build -I${SRCDIR}/onnx -O3 -Wall -g -DONNX_NAMESPACE=onnx
 // #cgo CXXFLAGS: -Wno-sign-compare -Wno-unused-function
 import "C"
